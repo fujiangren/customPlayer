4003ef6c9c30da6833897f5ea21c79fbb990bf4f
renfujiang_123 <rfj18610359126@163.com>
2019-04-12 21:52:22 +0800

    commit

---
+++ fujiangCustomPlayer/app/src/main/java/com/fujiang/customplayer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fujiang.customplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// 自己写个播放器
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
